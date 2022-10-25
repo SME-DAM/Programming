f737ff6106864c936700edb1a96d9928d91a764f
Ryutaro <ruytaro@theseed.games>
2022-10-25 15:41:42 +0200

    añadidos primos

---
+++ src/NumeroPrimo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NumeroPrimo {
+
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		boolean primo = true;
+		int total=0;
+		for (int j =2;j<1000000;j++) {
+			primo=true;
+		//System.out.print("Introduzca un número: ");
+		int numero = j;//sc.nextInt();
+		for (int i = 2; i <= Math.sqrt(numero); i++) {
+			if(numero%i==0) {
+				primo=false;
+				break;
+			}
+		}
+		if (primo)total++;
+		//System.out.printf("El numero %d %ses primo!\n",numero,primo?"":"no ");
+		}
+		System.out.printf("He encontrado %d primos!",total);
+	}
+}
--- src/Piramide.java
+++ src/Piramide.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Priramide {
+public class Piramide {
 
 	private static Scanner sc;
 

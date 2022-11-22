2a5850f2e4e5863cbe8922a1f0437fb078f84abb
Ryutaro <ruytaro@theseed.games>
2022-11-22 20:45:33 +0100

    matrices 2

--- Tema3/src/tema3/DiagonalUno.java
+++ Tema3/src/tema3/DiagonalUno.java
@@ -12,10 +12,13 @@ public class DiagonalUno {
 		sc = new Scanner(System.in);
 		System.out.println("Introduce lado:");
 		int side = sc.nextInt();
-		int matrix[][]= MyMatrix.generateMatrix(side, side, 11, 99);
-		for(int i = 0; i < side; i++) {
-			matrix[i][i]=1;
-			matrix[i][side-1-i]=1;
+		int erval = 100 - 2 + 1;
+		int[][] matrix= new int[side][side];
+		for (int r = 0; r < side; r++) {
+			for (int c = 0; c < side; c++) {
+				if(c == r || c == (side-r))
+				matrix[c][r]= (int) (Math.random() * erval) + 2;
+			}	
 		}
 		MyMatrix.printMatrix(matrix, side, side);
 		
